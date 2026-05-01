536ccf01be8aa9fc57b7bdd827ffaaaafc21891c
Maheli Samaraweera <mahelisamaraweera10@gmail.com>
2026-05-01 14:14:34 +0530

    initialize backend setup with Express server
    setup project structure, app.js, Mongodb connection and Installed dependencies

---
+++ config/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/financeDB");
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
